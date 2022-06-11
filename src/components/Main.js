b5b20746e83561573294523c1372d2dd96322d1b
Lilia Busowa <liliabusowa@yandex.ru>
2022-06-12 00:33:50 +0700

    6. Подключите API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,40 @@
-import React from "react";
-import cousteau from '../images/cousteau.jpg';
+import React, { useEffect } from "react";
+import apiconfig from '../utils/api';
 
 function Main(props) {
+  // Переменные состояния полей профиля пользователя (имя, о себе, аватар)
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  useEffect(() => {
+    apiconfig
+      .getUserInfo()
+      .then(userData => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+        return userData;
+      })
+  }, []);
+  
   return (
     <main>
       <section className="profile page__section">
         <div className="profile__avatar-container">
           <img 
             className="profile__avatar" 
-            src={cousteau}
+            src={userAvatar}
             alt="Фотография-аватар пользователя" 
           />
           <div className="profile__avatar-overlay" onClick={props.onEditAvatar} />
         </div>
         <div className="profile__info">
           <div className="profile__name-container">
-            <h1 className="profile__name">Жак-Ив Кусто</h1>
+            <h1 className="profile__name">{userName}</h1>
             <button className="profile__edit-button buttons" onClick={props.onEditProfile} />
           </div>
-          <p className="profile__job">Исследователь океана</p>
+          <p className="profile__job">{userDescription}</p>
         </div>
         <button type="button" className="profile__add-button buttons" onClick={props.onAddPlace} />
       </section>
